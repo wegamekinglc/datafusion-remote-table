e71ee202c09bced3aee747d86229dc751a905cb6
张林伟 <lewiszlw520@gmail.com>
2025-03-18 15:54:52 +0800

    Bump version

--- remote-table/src/connection/mod.rs
+++ remote-table/src/connection/mod.rs
@@ -23,6 +23,7 @@ pub trait Pool: Debug + Send + Sync {
 
 #[async_trait::async_trait]
 pub trait Connection: Debug + Send + Sync {
+    // TODO could add limit 1 to query
     async fn infer_schema(
         &self,
         sql: &str,
