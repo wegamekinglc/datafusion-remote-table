3e273df34403cbc4d3b2adf967ae643be9daee43
wegamekinglc <scrappedprince.li@gmail.com>
2025-06-01 15:40:38 +0800

    change example

--- remote-table/examples/postgres.rs
+++ remote-table/examples/postgres.rs
@@ -1,21 +1,38 @@
 use datafusion::prelude::SessionContext;
 use datafusion_remote_table::{ConnectionOptions, PostgresConnectionOptions, RemoteTable};
 use std::sync::Arc;
+use datafusion::execution::options::CsvReadOptions;
+use tokio::time::Instant;
+
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let options = ConnectionOptions::Postgres(PostgresConnectionOptions::new(
         "localhost",
         5432,
-        "user",
-        "password",
+        "postgres",
+        "we083826",
     ));
-    let remote_table = RemoteTable::try_new(options, "select * from supported_data_types").await?;
+
+    let remote_table = RemoteTable::try_new(options,
+                                                    "select foo, bar, id from public.pg_test_data").await?;
 
     let ctx = SessionContext::new();
     ctx.register_table("remote_table", Arc::new(remote_table))?;
+    ctx.register_csv("csv_table",
+                     "D:/dev/github/datafusion-flight-sql-server/datafusion-flight-sql-server/examples/test.csv",
+                     CsvReadOptions::new()).await?;
 
-    ctx.sql("select * from remote_table").await?.show().await?;
+    let sql = "select count(1) from remote_table as a, csv_table as b where a.foo = 'a' AND a.foo = b.foo";
+
+    // warm up
+    ctx.sql(sql).await?;
+
+    // time the execution
+    let start = Instant::now();
+    ctx.sql(sql).await?.show().await?;
+    let duration = start.elapsed();
+    println!("Time elapsed in query is: {:?}", duration);
 
     Ok(())
 }
